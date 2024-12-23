08d7369957f2171b8d75f57790d48c71fcc372d4
Nikolie <46843801+supNir@users.noreply.github.com>
2024-12-23 16:40:40 +0300

    nlog lab

---
+++ Prac_2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Prac_2
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var p = new Program();
+            double x = 519313212;
+            p.multiplication(x);
+        }
+
+        public void multiplication(double n)
+        {
+            try
+            {
+                while (true)
+                {
+                    n *= n;
+                    if (double.IsInfinity(n))
+                    {
+                        throw new OverflowException("Переполнение при умножении");
+                    }
+                    Console.WriteLine(n);
+                }
+            }
+            catch (OverflowException overE)
+            {
+                Console.WriteLine(overE.Message);
+            }
+        }
+    }
+}
